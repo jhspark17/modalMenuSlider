967b685cf137739e655db9dde0d7b1944e447cac
Joseph Park <jhspark17@gmail.com>
2020-07-06 19:45:01 -0700

    Add function

--- script.js
+++ script.js
@@ -1,15 +1,41 @@
-const btn = document.getElementById("btn");
-const modal = document.getElementById("myModal")
-const closeModal = document.getElementById('close-modal');
+// const btn = document.getElementById("btn");
+// const modal = document.getElementById("myModal")
+// const closeModal = document.getElementById('close-modal');
 
 
 
 
 
-btn.addEventListener('click', () => {
-  modal.style.display = "block"
-})
+// btn.addEventListener('click', () => {
+//   modal.style.display = "block"
+// })
 
-closeModal.addEventListener('click', () => {
-  modal.style.display = "none"
-})
+// closeModal.addEventListener('click', () => {
+//   modal.style.display = "none"
+// })
+
+var restoreIpAddresses = function(s) {
+  const res = [];
+  
+  const backtrack = (temp, str) => {
+    if (temp.length > 4) return;
+    if (!str.length && temp.length === 4) {
+      res.push(temp.join("."));
+      return;
+    }
+    
+    for (let i = 0; i < str.length; i++) {
+      let vis = str.slice(0, i + 1);
+      if (vis[0] === "0" && vis.length > 1) continue;
+      vis = parseInt(vis);
+      if (vis > 255 || vis < 0) continue;
+      backtrack([...temp, vis], str.slice(i + 1));
+    }
+  }
+  
+  backtrack([], s);
+  return res;
+};
+
+
+console.log(restoreIpAddresses("010010"))
